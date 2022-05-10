49979af3c6ea6c555956c2a5c793cfb082c38bac
Ertugrul Yondem <ertugrulyondem@gmail.com>
2022-05-10 21:48:59 +0300

    Initial Commit

---
+++ Project2_GuestTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2_GuestTheFlag
+//
+//  Created by Ertuğrul Yöndem on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
